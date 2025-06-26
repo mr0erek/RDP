0e64c1ab9061eb9f7a424a1ab4b683a14a89ad15
S P <88350720+mr0erek@users.noreply.github.com>
2025-06-26 21:25:33 +0530

    Update Download.bat

--- Download.bat
+++ Download.bat
@@ -15,7 +15,7 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
-set password=@#Drk123456
+set password=Drk7
 :: -----------------Coded by Mr.Dereķ - @E343IO---------------------
 :: Attempt to import the module
 powershell.exe -command "Import-Module Microsoft.PowerShell.Security"
@@ -39,4 +39,4 @@ powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-Secu
 start "" "rustdesk.exe"
 python login.py
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
-tzutil /s "Sri Lanka Standard Time"
+tzutil /s "India Standard Time"
